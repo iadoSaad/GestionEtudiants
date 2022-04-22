3b4e07e93df8d09c4bfbe6568905945afea68780
mac <mac@controller.access.network>
2022-04-22 09:34:14 +0000

    Redéfinition de add

--- GestionEtudiants/src/aiac/gi18/coursjava/ListEtudiants.java
+++ GestionEtudiants/src/aiac/gi18/coursjava/ListEtudiants.java
@@ -4,4 +4,12 @@ import java.util.ArrayList;
 
 public class ListEtudiants extends ArrayList<Etudiant> {
 
+	
+	@Override
+	public boolean add(Etudiant e) {
+		int index=this.indexOf(e);
+		if(index<0) return super.add(e);
+		this.set(index, e);
+		return true;
+	}
 }
