c8c0de535543644bc030aa55a64613afb6a7b391
mac <mac@controller.access.network>
2022-04-22 09:22:00 +0000

    Création de constructeur

--- GestionEtudiants/src/aiac/gi18/coursjava/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18/coursjava/Etudiant.java
@@ -4,5 +4,12 @@ public class Etudiant {
 	private int id;
 	private String nom;
 	private double note;
+	
+	public Etudiant(int id,String nom){
+		this.id=id;
+		this.nom=nom;
+		this.note=10;
+	}
+	
 
 }
