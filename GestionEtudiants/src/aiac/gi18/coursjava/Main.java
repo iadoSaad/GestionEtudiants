1dacd291db0491d4f33827af13756ca61b5355fe
mac <mac@controller.access.network>
2022-04-22 09:50:39 +0000

    Creation d'etudiants

--- GestionEtudiants/src/aiac/gi18/coursjava/Main.java
+++ GestionEtudiants/src/aiac/gi18/coursjava/Main.java
@@ -3,7 +3,12 @@ package aiac.gi18.coursjava;
 public class Main {
 
 	public static void main(String[] args) {
-
+		// Créer 3 Etudiants e1(1, ‘’hamid’’ ), e2(2, ‘’driss’’) et e3(1, ‘’youssef’’) .
+		Etudiant e1 = new Etudiant(1, "hamid"), 
+				e2 = new Etudiant(2, "driss"), 
+				e3 = new Etudiant(1, "Youssef");
+		
+	
 	}
 
 }
